6fb7d3c03c908bc30bd68540680b8900df1cbcfc
Valeriy Kulagin <kulagin.valeriy@gmail.com>
2023-02-07 22:53:21 +0300

    Variable names chsnged

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,12 +1,12 @@
 public class CreditPaymentService {
 
-    public int calculate(int a, int b) {
+    public int calculate(int amountOfCredit, int numberOfMonths) {
         double result;
         double percent = 9.9 / 12 / 100; // месячная процентная ставка
-        double pow = Math.pow(1 + percent, b);
+        double pow = Math.pow(1 + percent, numberOfMonths);
         double coff = (percent * pow) / (pow -1); // коэффициент аннуитента
 
-        result = a * coff;
+        result = amountOfCredit * coff;
 
         return (int)result;
     }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        int credit = 1_000_000; //сумма кредита
-        int date = 12; // срок кредита в месяцах
+        int credit = 2_000_000; //сумма кредита
+        int date = 24; // срок кредита в месяцах
 
         double payment = service.calculate(credit, date);
         System.out.println("Ежемесячный платеж = " + payment);
